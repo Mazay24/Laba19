11cc8b6cc6c0a0fabfefea7b1a222aa171e990b0
Исаев Никита <96546414+Mazay24@users.noreply.github.com>
2022-05-28 10:49:35 +0300

    Update Main.java

--- src/com/metanit/Main.java
+++ src/com/metanit/Main.java
@@ -4,30 +4,7 @@ package com.metanit;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
-    public static void main(String[] args) {
-        Groups RDR2 = new Groups();
-        Groups CS16 = new Groups();
-        Groups MEMES = new Groups();
-        Groups JAVA = new Groups();
 
-        USERS subscriber1 = new USERS("Никита Исаев Александрович");
-        USERS subscriber2 = new USERS("Матвей Марусик Даниилович");
-        RDR2.regObserver(subscriber1);
-        RDR2.regObserver(subscriber2);
-        CS16.regObserver(subscriber1);
-        CS16.regObserver(subscriber2);
-        MEMES.regObserver(subscriber1);
-        MEMES.regObserver(subscriber2);
-        JAVA.regObserver(subscriber2);
-
-        RDR2.setNews("Рокстар услышали мольбы игроков и добавили в RDR online режим ограбления банков" + "\n");
-        CS16.setNews("S1mple показал новую раскитку на карте DE_DUST!" + "\n");
-        MEMES.setNews("Колобок повесился!!!" + "\n");
-        JAVA.setNews("Никита Исаев сделал лабораторную работу по Патерну Наблюдатель и расчитывает получить 5!" + "\n");
-
-    }
-}
 interface Observable {
     void notifyObservers();
     void regObserver(Observer o);
@@ -64,5 +41,29 @@ class USERS implements Observer{
     @Override
     public void Notification(String news) {
         System.out.println(name + " Получил оповещение: " + news);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Groups RDR2 = new Groups();
+        Groups CS16 = new Groups();
+        Groups MEMES = new Groups();
+        Groups JAVA = new Groups();
+
+        USERS subscriber1 = new USERS("Никита Исаев Александрович");
+        USERS subscriber2 = new USERS("Матвей Марусик Даниилович");
+        RDR2.regObserver(subscriber1);
+        RDR2.regObserver(subscriber2);
+        CS16.regObserver(subscriber1);
+        CS16.regObserver(subscriber2);
+        MEMES.regObserver(subscriber1);
+        MEMES.regObserver(subscriber2);
+        JAVA.regObserver(subscriber2);
+
+        RDR2.setNews("Рокстар услышали мольбы игроков и добавили в RDR online режим ограбления банков" + "\n");
+        CS16.setNews("S1mple показал новую раскитку на карте DE_DUST!" + "\n");
+        MEMES.setNews("Колобок повесился!!!" + "\n");
+        JAVA.setNews("Никита Исаев сделал лабораторную работу по Патерну Наблюдатель и расчитывает получить 5!" + "\n");
+
     }
 }
